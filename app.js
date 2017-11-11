59c1498b63a490c0cc182b37a8464fba97b877ef
Mandy Tong <mandytong@CroboMBA3s-MacBook-Pro.local>
2017-11-10 23:36:09 -0800

    set up html, css, js add gameboard and columns

---
+++ app.js
@@ -0,0 +1,39 @@
+console.log("connect four");
+
+// < ---------- create game board ---------->
+
+const $gameBoard = $("div").addClass("gameBoard");
+$(".container").append($gameBoard);
+
+
+
+
+
+// < ---------- create rows ---------->
+
+
+
+
+
+
+// < ---------- create columns ---------->
+
+
+const $column1 = $("<div>").addClass("column").attr("id", "column1");
+const $column2 = $("<div>").addClass("column").attr("id", "column2");
+const $column3 = $("<div>").addClass("column").attr("id", "column3");
+const $column4 = $("<div>").addClass("column").attr("id", "column4");
+const $column5 = $("<div>").addClass("column").attr("id", "column5");
+const $column6 = $("<div>").addClass("column").attr("id", "column6");
+const $column7 = $("<div>").addClass("column").attr("id", "column7");
+
+
+// < ---------- start game ---------->
+
+// < ---------- event listeners ---------->
+
+// < ---------- on-click events ---------->
+
+// < ---------- score board ---------->
+
+// < ---------- end game ---------->
