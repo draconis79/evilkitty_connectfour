9efc2a6d68a71f9cec9b1d108ff77d8d858eb399
Mandy Tong <mandytong@CroboMBA3s-MacBook-Pro.local>
2017-11-15 16:02:14 -0800

    check win from vertical and horizontal

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
 $(() => {
 
     const createBoard = () => {
-        // create a column with 7 rows
+        // create a gameboard
         // change camel case on id class to sausage-case
         const $gameBoard = $("<div>").addClass("gameBoard");
         for (let i = 0; i < 6; i++) {
             // create 6 rows on gameboard
             const $row = $("<div>").addClass("row");
             $row.attr("id", "row" + (i + 1));
+            // appending row to the gameboard
             $gameBoard.append($row);
             for (let j = 0; j < 7; j++) {
 
                 // create game slots on gameboard
                 const $circle = $("<div>").addClass("circle");
                 $circle.addClass('empty');
-                // setting up attribute for column to isolate column to do something.
+                // setting up attribute for each column to isolate column to do something later.
                 $circle.attr("row", (i + 1));
                 $circle.attr("column", (j + 1));
                 $circle.attr("id", ("row" + (i + 1) + "column" + (j + 1)));
@@ -22,6 +23,7 @@ $(() => {
 
             }
         }
+        // appending gameboard to the container div
         $(".container").append($gameBoard);
     }
 
@@ -32,28 +34,133 @@ $(() => {
 
 
 
-
+    // variables
     let numMagSlots = 0;
     let numGreySlots = 0;
 
     // creating an argument slotChecked, whenever addColor is called, it will take this variable. Parameter slotChecked
+    // allows game to alternate turns (colors) from each player
 
     const addColor = (slotChecked) => {
-        if (numMagSlots <= numGreySlots) {
+        if (numMagSlots !== numGreySlots) {
             $(slotChecked).css("background-color", "darkmagenta").addClass('player1');
-            // increment number by 1 so that we know there's one more magenta slot, also adding a class.
+            // increment number by 1 so that we know there's one more magenta slot, also adding a class of player1.
             numMagSlots++;
+            checkForWinner();
             // console.log('player1');
-            // console.log($('.player1'));
             // if there are more magenta slots than grey slots, then color slots we are checking to grey.
         } else {
             $(slotChecked).css("background-color", "darkgrey").addClass('player2');
-
+            // increment number by 1 so that we know there's one more dark grey slot, also adding a class of player2.
             numGreySlots++;
             // console.log('player2');
-            // console.log($('.player2'));
+            checkForWinner();
+            // break;
         }
     }
+
+
+    // check if there are 4 color slots that match up in rows and columns
+    // check circle next to where circle was clicked left, right, up, and down, if it's the same class, check again, until it reaches four of the same has class player 1 or player 2.
+    // everytime a game piece is clicked, check to see if player has connect four.
+
+    // takes argument row and column
+    // const checkForWinner = (row, column) {
+    //   checkDirection(row);
+    //   checkDirection(column);
+      // lets say we click on row 2, column 2, we need to check in the direction up and down
+
+
+      const checkDirection = (direction) => {
+      // check if color that was just clicked matches the color we are looking for
+      // keeping track of total of slots
+      let total = 0;
+      // row that is passed in and in the i direction
+      let i = $(gameboard + direction.i);
+      // column that is passed in and in the i direction
+      let j = $($row + direction.j);
+
+      while (i >= 0 && i < rows && j >= 0 && j < gameboard)
+          total++;
+      i += direction.i;
+      j += direction.j;
+      return total;
+  }
+
+
+  // direction A = up
+  // direction B = down
+  const checkForWinner = (directionA, directionB) => {
+      const total = 1 +
+          // everything above
+          checkDirection(directionA) +
+          // everything below
+          checkDirection(directionB);
+      if (total >= 4) {
+          return player1;
+      } else {
+          return null;
+      }
+  }
+  // row direction from up to down
+  const checkUpDown = () => {
+      // pass in i of -1 and j of 0, pass in a direction going up (i: -1, j:0), then down (i: 1, j:0)
+      return checkWinner({
+          i: -1,
+          j: 0
+      }, {
+          i: 1,
+          j: 0
+      })
+  }
+  // row direction from left to right
+  const checkLeftRight = () => {
+      // pass in i of -1 and j of 0, pass in a direction going left and right)
+      return checkWinner({
+          i: 0,
+          j: -1
+      }, {
+          i: 0,
+          j: 1
+      })
+  }
+
+  const checkDiagonalBottomLeftToTopRight = () => {
+      return checkWin({
+          i: 1,
+          j: -1
+      }, {
+          i: 1,
+          j: 1
+      });
+  }
+
+  const checkDiagonalTopLeftToBottomRight = () => {
+      return checkWin({
+          i: 1,
+          j: 1
+      }, {
+          i: -1,
+          j: -1
+      });
+  }
+
+checkVerticals() || checkHorizontals() || checkDiagonalBottomLeftToTopRight() || checkDiagonalTopLeftToBottomRight();
+
+
+    //
+    //
+    // // place this higher than function
+    //       const winner = checkWinRowColumn (row, column)
+    //         if (winner) {
+    //           // do something here
+    //           alert("message for winning player here")
+    //           return;
+    //         }
+
+
+
+
     // creating event listener to listen to click that has the circle class. starts anonymous function that takes the argument event.
 
     $(".circle").on("click", (event) => {
